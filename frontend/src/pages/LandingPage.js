8b333f09713fd6059497c2724b2b35ba40fe8e9c
HimeshRasaily <himeshrasaily010@gmail.com>
2026-02-09 12:02:04 +0530

    debug: isolate Navigation component

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,26 +1,10 @@
 import React from "react";
 import Navigation from "../components/Navigation";
-import HeroSection from "../components/HeroSection";
-import SkillCategories from "../components/SkillCategories";
-import MentorsSection from "../components/MentorsSection";
-import CaseStudiesSection from "../components/CaseStudiesSection";
-import TestimonialsSection from "../components/TestimonialsSection";
-import DashboardPreview from "../components/DashboardPreview";
-import PricingSection from "../components/PricingSection";
-import Footer from "../components/Footer";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
-      <HeroSection />
-      <SkillCategories />
-      <MentorsSection />
-      <CaseStudiesSection />
-      <TestimonialsSection />
-      <DashboardPreview />
-      <PricingSection />
-      <Footer />
     </div>
   );
 }
