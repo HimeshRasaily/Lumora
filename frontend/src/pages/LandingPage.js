7eafb7e1a0cd6311720410da09f4db5df3bdd02c
HimeshRasaily <himeshrasaily010@gmail.com>
2026-02-09 11:55:13 +0530

    debug: isolate landing page render

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
-import Navigation from '../components/Navigation';
-import HeroSection from '../components/HeroSection';
-import SkillCategories from '../components/SkillCategories';
-import MentorsSection from '../components/MentorsSection';
-import CaseStudiesSection from '../components/CaseStudiesSection';
-import TestimonialsSection from '../components/TestimonialsSection';
-import DashboardPreview from '../components/DashboardPreview';
-import PricingSection from '../components/PricingSection';
-import Footer from '../components/Footer';
+import React from "react";
 
 export default function LandingPage() {
-  const { scrollYProgress } = useScroll();
-  const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
-
   return (
-    <div className="min-h-screen bg-background">
-      <Navigation />
-      <HeroSection />
-      <SkillCategories />
-      <MentorsSection />
-      <CaseStudiesSection />
-      <TestimonialsSection />
-      <DashboardPreview />
-      <PricingSection />
-      <Footer />
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      Landing page mounted successfully
     </div>
   );
 }
